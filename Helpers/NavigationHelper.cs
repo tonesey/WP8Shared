119ab509dde9ca0acc02318cd899475bf4a005d5
davide.tentori@hotmail.com <davide.tentori@hotmail.com>
2014-01-15 22:35:05 +0100

    NavigationService introduction

--- Helpers/NavigationHelper.cs
+++ Helpers/NavigationHelper.cs
@@ -1,32 +1,63 @@
-﻿using System;
-using System.Net;
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Windows.Navigation;
-using System.Windows.Threading;
+using Wp8Shared.Helpers;
 
 namespace Wp8Shared.Helpers
 {
-    public class NavigationHelper
+
+    public class NavigationHelper : INavigationService
     {
-        public static void SafeNavigateTo(NavigationService ns, Dispatcher ds, string pageUrl)
+        private PhoneApplicationFrame _mainFrame;
+
+        public event NavigatingCancelEventHandler Navigating;
+
+        public void NavigateTo(Uri pageUri)
         {
-            ds.BeginInvoke(() =>
+            if (EnsureMainFrame())
             {
-                try
+                _mainFrame.Navigate(pageUri);
+            }
+        }
+
+        public void GoBack()
+        {
+            if (EnsureMainFrame()
+                && _mainFrame.CanGoBack)
+            {
+                _mainFrame.GoBack();
+            }
+        }
+
+        private bool EnsureMainFrame()
+        {
+            if (_mainFrame != null)
+            {
+                return true;
+            }
+
+            _mainFrame = Application.Current.RootVisual as PhoneApplicationFrame;
+
+            if (_mainFrame != null)
+            {
+                // Could be null if the app runs inside a design tool
+                _mainFrame.Navigating += (s, e) =>
                 {
-                    ns.Navigate(new Uri(pageUrl, UriKind.Relative));
-                }
-                catch (InvalidOperationException)
-                {
-                }
-            });
+                    if (Navigating != null)
+                    {
+                        Navigating(s, e);
+                    }
+                };
+
+                return true;
+            }
+            return false;
         }
     }
+
 }
